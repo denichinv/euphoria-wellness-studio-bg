03a976cf6d5dbc079f244c15afe335c873d0e2f9
Vilizar Denichin <denichinv@gmail.com>
2026-02-06 17:15:35 +0000

    add about section accessibility tests

---
+++ src/components/sections/About.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { About } from "./About";
+
+describe("About", () => {
+  beforeEach(() => {
+    render(<About />);
+  });
+
+  test("should have semantic header (h2) ", () => {
+    expect(screen.getByRole("heading", { level: 2 })).toBeInTheDocument();
+  });
+
+  test("renders the section with correct aria-labelledby", () => {
+    const section = screen.getByRole("region");
+    expect(section).toHaveAttribute("aria-labelledby", "about-title");
+    expect(section).toHaveAttribute("id", "about");
+  });
+});
