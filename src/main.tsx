9957039d254aeadb684a45b12fb8f031a8b0bd20
Vilizar Denichin <denichinv@gmail.com>
2026-03-11 20:23:20 +0000

    connect LanguageProvider to app root

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {
+  LanguageContext,
+  LanguageProvider,
+} from "./context/LanguageContext.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </StrictMode>,
-)
+);
