1fff0df991b0b871586f9f9e47f052503a7b0b08
Vilizar Denichin <denichinv@gmail.com>
2026-03-11 20:18:31 +0000

    add LanguageProvider and useLanguage hook

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,3 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState, useContext } from "react";
 
-export const LanguageContext = createContext(null);
+type LanguageContextType = {
+  language: string;
+  setLanguage: (lang: string) => void;
+};
+export const LanguageContext = createContext<LanguageContextType | null>(null);
+
+export function LanguageProvider({ children }: { children: React.ReactNode }) {
+  const [language, setLanguage] = useState("bg");
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage must be used within a LanguageProvider");
+  }
+  return context;
+}
